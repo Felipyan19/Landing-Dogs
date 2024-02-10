83734fb4907d2f6f202790f901b3dd8210175533
fabricadesoftware1 <t7tn7hhamjjw22bnfu3i7c3qxdtyo7lf42dwhdogupoxfu2eeryq>
2024-02-10 13:26:32 -0500

    Estructura: arquitectura. AC

---
+++ script.js
@@ -0,0 +1,10 @@
+const images = [
+    "./assets/pexels-dominika-roseclay-2023384.jpg",
+    "./assets/pexels-dominika-roseclay-2023384.jpg",
+    "./assets/pexels-dominika-roseclay-2023384.jpg",
+    "./assets/pexels-dominika-roseclay-2023384.jpg"
+  ];
+
+  images.forEach((image, index) => {
+
+  })
