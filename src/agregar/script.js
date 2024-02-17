9f4ed2892d2c27ffa3107df546e128ce94bc32ed
fabricadesoftware1 <t7tn7hhamjjw22bnfu3i7c3qxdtyo7lf42dwhdogupoxfu2eeryq>
2024-02-17 15:54:41 -0500

    Feature: Modal en agregar. AC

--- src/agregar/script.js
+++ src/agregar/script.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    const modalRegistro = () =>{
+      $('#registroExitosoModal').modal('show');
+    }
     const form = document.getElementById('form');
-  
+    
     form.addEventListener("submit", function(e) {
       e.preventDefault();
   
       if (form.checkValidity() === false) {
         e.stopPropagation();
+        form.classList.add('was-validated');
+        return;
       } else {
         const fields = Object.fromEntries(new FormData(e.target));
         const dogsData = JSON.parse(localStorage.getItem("Dogs")) || [];
-  
+           
+        const bodyModal = document.getElementById('modal-body')
+
+        bodyModal.innerHTML = `<div>
+        <p>Felicidades!<strong> ${fields.titulo}${' '}</strong>ha sido registrado correctamente</p>
+        <img src=${fields.imagen} class="w-100" >
+        <h3>${fields.titulo}</h3>
+        <p>${fields.contenido}</p>
+        `
+
         if (dogsData) {
           localStorage.removeItem("Dogs");
           localStorage.setItem("Dogs", JSON.stringify([...dogsData, fields]));
@@ -17,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
           localStorage.setItem("Dogs", JSON.stringify([fields]));
         }
       }
-  
+      modalRegistro();
       form.classList.add('was-validated');
     });
   });
